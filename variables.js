b8579a3c0e1ecebbc6ea20877647b1a6051a9db3
Nina Brissey <nbrissey@gmail.com>
2021-04-02 13:04:57 -0700

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,11 @@
+var movie = 'Billy Madison';
+var isEntertaining = true;
+movie = 'Gladiator';
+var num4 = 7;
+var num6 = 10;
+var product = num6 * num4;
+var favoriteFoods = ['pizza', 'salad', 'Sour Patch Kids'];
+
+favoriteFoods.splice(0, 1);
+
+console.log(movie.length);
