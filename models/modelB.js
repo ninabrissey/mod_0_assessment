4afa7119a1c3cad2c778321ab820b6360eaca45e
Nina Brissey <nbrissey@gmail.com>
2021-04-02 13:26:07 -0700

    Add object developer

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: 'Nina Brissey',
+  school: 'Turing',
+  yearsExperience: .08,
+  monthsToCompletion: 7,
+  accepted: true,
+  paid: false,
+  qualities: ['hard-working', 'creative', 'persistant', 'problem-solver'],
+  needsGrowth: ['calendar', 'review', 'organization']
+}
