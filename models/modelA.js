3a3423b85ef5916d9d6b3b8968298c2e766f3ab7
Nina Brissey <nbrissey@gmail.com>
2021-04-02 13:18:18 -0700

    Add object dinosaur

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: 'Tyrannosaurus',
+  nickname: 'T-Rex',
+  avgHeight: 40,
+  avgWeight: 13000,
+  prehistoric: true,
+  scary: true,
+  features: ['claws', 'sharp teeth', 'small arms', 'powerful bite'],
+  movies: ['Jurassic Park', 'Jurassic World', 'Planet of Dinosaurs']
+}
